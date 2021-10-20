c58ab1c8c9b80303d1ba1c32d24eb5007f736210
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-10-20 10:31:55 +0000

    nn_modules: add bottleneck with progressive dilation

--- fnet/nn_modules/fnet_nn_3d.py
+++ fnet/nn_modules/fnet_nn_3d.py
@@ -2,5 +2,5 @@ import fnet.nn_modules.fnet_nn_3d_params
 
 
 class Net(fnet.nn_modules.fnet_nn_3d_params.Net):
-    def __init__(self, depth=4, mult_chan=32):
-        super().__init__(depth=depth, mult_chan=mult_chan)
+    def __init__(self, depth=4, mult_chan=32, dilate=1):
+        super().__init__(depth=depth, mult_chan=mult_chan, dilate=dilate)
--- fnet/nn_modules/fnet_nn_3d_params.py
+++ fnet/nn_modules/fnet_nn_3d_params.py
@@ -2,15 +2,16 @@ import torch
 
 
 class Net(torch.nn.Module):
-    def __init__(self, depth=4, mult_chan=32, in_channels=1, out_channels=1):
+    def __init__(self, depth=4, mult_chan=32, dilate=1, in_channels=1, out_channels=1):
         super().__init__()
         self.depth = depth
         self.mult_chan = mult_chan
+        self.dilate = dilate
         self.in_channels = in_channels
         self.out_channels = out_channels
 
         self.net_recurse = _Net_recurse(
-            n_in_channels=self.in_channels, mult_chan=self.mult_chan, depth_parent=self.depth, depth=self.depth
+            n_in_channels=self.in_channels, mult_chan=self.mult_chan, depth_parent=self.depth, depth=self.depth, dilate = self.dilate
         )
         self.conv_out = torch.nn.Conv3d(
             self.mult_chan, self.out_channels, kernel_size=3, padding=1
@@ -22,7 +23,7 @@ class Net(torch.nn.Module):
 
 
 class _Net_recurse(torch.nn.Module):
-    def __init__(self, n_in_channels, mult_chan=2, depth_parent=0, depth=0):
+    def __init__(self, n_in_channels, mult_chan=2, depth_parent=0, depth=0, dilate=1):
         """Class for recursive definition of U-network.p
 
         Parameters
@@ -34,6 +35,8 @@ class _Net_recurse(torch.nn.Module):
         depth
             If 0, this subnet will only be convolutions that double the channel
             count.
+        dilate
+            Number of blocks in bottleneck with progressive dilation.
 
         """
         super().__init__()
@@ -45,8 +48,10 @@ class _Net_recurse(torch.nn.Module):
         else:
             n_out_channels = n_in_channels * mult_chan
 
-        self.sub_2conv_more = SubNet2Conv(n_in_channels, n_out_channels)
-        if depth > 0:
+        if depth == 0:
+            self.sub_2conv_more = DilatedBottleneck(n_in_channels, n_out_channels, depth=dilate)
+        elif depth > 0:
+            self.sub_2conv_more = SubNet2Conv(n_in_channels, n_out_channels)
             self.sub_2conv_less = SubNet2Conv(2 * n_out_channels, n_out_channels)
             self.conv_down = torch.nn.Conv3d(
                 n_out_channels, n_out_channels, 2, stride=2
@@ -96,3 +101,27 @@ class SubNet2Conv(torch.nn.Module):
         x = self.bn2(x)
         x = self.relu2(x)
         return x
+
+    
+class DilatedBottleneck(torch.nn.Module):
+    def __init__(self, n_in, n_out, depth=4):
+        super().__init__()
+
+        for i in range(depth):
+            dilate = 2 ** i
+            model = [
+                torch.nn.Conv3d(n_in, n_out, kernel_size=3, padding=dilate, dilation=dilate),
+                torch.nn.BatchNorm3d(n_out),
+                torch.nn.ReLU(inplace=True)
+            ]
+            self.add_module('bottleneck%d' % (i + 1), torch.nn.Sequential(*model))
+            if i == 0:
+                n_in = n_out
+
+    def forward(self, x):
+        bottleneck_output = 0
+        output = x
+        for _, layer in self._modules.items():
+            output = layer(output)
+            bottleneck_output += output
+        return bottleneck_output
