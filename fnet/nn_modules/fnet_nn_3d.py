40545505660b30285ed26308f41dc6dbadaa5795
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2021-10-20 10:26:10 +0000

    fix(nn_modules): enable chaging nn depth and width

--- fnet/nn_modules/fnet_nn_3d.py
+++ fnet/nn_modules/fnet_nn_3d.py
@@ -2,5 +2,5 @@ import fnet.nn_modules.fnet_nn_3d_params
 
 
 class Net(fnet.nn_modules.fnet_nn_3d_params.Net):
-    def __init__(self):
-        super().__init__(depth=4, mult_chan=32)
+    def __init__(self, depth=4, mult_chan=32):
+        super().__init__(depth=depth, mult_chan=mult_chan)
