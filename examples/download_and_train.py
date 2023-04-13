33c3a6b80f43a59fc5136b927b481d45fec6617b
alxndrkalinin <1107762+alxndrkalinin@users.noreply.github.com>
2023-04-13 01:08:26 +0000

    example: set train_fraction to .6 as per paper

--- examples/download_and_train.py
+++ examples/download_and_train.py
@@ -32,7 +32,7 @@ args = parser.parse_args()
 
 gpu_id = args.gpu_id
 n_images_to_download = args.n_imgs  # more images the better
-train_fraction = 0.75
+train_fraction = 0.6 # see Supplementary Table 1
 
 image_save_dir = "{}/".format(os.getcwd())
 model_save_dir = "{}/model/".format(os.getcwd())
